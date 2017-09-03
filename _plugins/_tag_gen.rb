338dab0c35a501e28e83e6903640e0b78b617a91
apemix <heyalumnicalvari@gmail.com>
2017-09-03 10:44:43 +0700

    Update _tag_gen.rb

--- _plugins/_tag_gen.rb
+++ _plugins/_tag_gen.rb
@@ -1,33 +1,82 @@
 module Jekyll
-  class TagIndex < Page
-    def initialize(site, base, dir, tag)
+  # Base class for the tag pages with all the shared behaviour
+  class BaseTagPage < Page
+    def initialize(site, base, dir)
       @site = site
       @base = base
       @dir = dir
       @name = 'index.html'
-      self.process(@name)
-      self.read_yaml(File.join(base, '_layouts'), 'tag_index.html')
-      self.data['tag'] = tag
-      tag_title_prefix = site.config['tag_title_prefix'] || 'Posts Tagged &ldquo;'
-      tag_title_suffix = site.config['tag_title_suffix'] || '&rdquo;'
-      self.data['title'] = "#{tag_title_prefix}#{tag}#{tag_title_suffix}"
+      self.process @name
+      self.read_yaml File.join(base, '_layouts'), layout_page
+    end
+
+    # Implement this function in child classes
+    # Expected to return a string containing the filename of the layout in _layouts
+    def layout_page
+      raise
     end
   end
-  class TagGenerator < Generator
+
+  # Represents a specific tag page
+  class TagPage < BaseTagPage
+    def initialize(site, base, dir, tag)
+      super(site, base, dir)
+      self.data['tag'] = tag
+    end
+
+    def layout_page
+      'tag_page.html'
+    end
+  end
+
+  # Generate a page for every tag
+  class TagPageGenerator < Generator
     safe true
+
     def generate(site)
-      if site.layouts.key? 'tag_index'
-        dir = site.config['tag_dir'] || 'tag'
+      if site.layouts.has_key? 'tag_page'
+        dir = site.config['tag_page_dir'] || 'tag'
         site.tags.keys.each do |tag|
-          write_tag_index(site, File.join(dir, tag), tag)
+          write_tag_page(site, File.join(dir, tag), tag)
         end
       end
     end
-    def write_tag_index(site, dir, tag)
-      index = TagIndex.new(site, site.source, dir, tag)
-      index.render(site.layouts, site.site_payload)
-      index.write(site.dest)
-      site.pages << index
+
+    private
+
+    def write_tag_page(site, dir, tag)
+      page = TagPage.new(site, site.source, dir, tag)
+      page.render(site.layouts, site.site_payload)
+      page.write(site.dest)
+      site.pages << page
+    end
+  end
+
+  # Represents a page with all the tags
+  class TagIndexPage < BaseTagPage
+    def layout_page
+      'tag_index.html'
+    end
+  end
+
+  # Generates a page with all tags
+  class TagIndexGenerator < Generator
+    safe true
+
+    def generate(site)
+      if site.layouts.has_key? 'tag_index'
+        dir = site.config['tag_index_dir'] || 'tags'
+        write_tag_index(site, dir)
+      end
+    end
+
+    private
+
+    def write_tag_index(site, dir)
+      page = TagIndexPage.new(site, site.source, dir)
+      page.render(site.layouts, site.site_payload)
+      page.write(site.dest)
+      site.pages << page
     end
   end
 end
